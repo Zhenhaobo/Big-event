f3e5b08ac7b3a3e6045e1b5dfde9b0336a597dea
zhenhaobo <zhbhenaoo@163.com>
2020-11-23 20:57:26 +0800

    完成了user分支

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,29 +1,37 @@
 $(function(){
     getuserinfo()
     // 获取用户的基本信息
-      function getuserinfo(){
-          $.ajax({
-              type: "GET",
-              url: "/my/userinfo",             
-              success: function(res){
-                  if( res.status !== 0) return console.log(res.message);
-                   if(res.data.user_pic == null) {
-                       $('.layui-nav-img').hide()
-                   }else {
-                    res.data.user_pic.attr('url',res.data.user_pic)
-                   }
-              },
-            //    如果在没权限的状态下登录 会强制回到登录页面
-              
-          })
-      }    
+       
     //   退出管理系统 清除 token
   $('#out').on('click',function(e) {
       localStorage.removeItem('token')
       e.preventDefault()
       location.href="/login.html"
   })
-  
-
-
 })
+   
+function getuserinfo(){
+  $.ajax({
+      type: "GET",
+      url: "/my/userinfo",             
+      success: function(res){
+        console.log(res);
+          if( res.status !== 0) return console.log(res.message);
+          let uname =res.data.nickname || res.data.nickname
+          if(res.data.username) $('.welcome').html('欢迎'+ uname)
+           if(res.data.user_pic == null) {
+            $('.layui-nav-img').hide()
+            let textimg = uname[0].toUpperCase()
+            $('.text-img').html(textimg).show()
+           }  
+           else  {
+            $('.layui-nav-img').attr('src',res.data.user_pic).show()
+            $('.text-img').hide()
+           }  
+               
+      }
+
+    //    如果在没权限的状态下登录 会强制回到登录页面
+      
+  })
+} 
